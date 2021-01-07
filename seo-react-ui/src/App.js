77cf9eedddcc398315a4197e639f417ddf006382
KirilIliev24 <43208740+KirilIliev24@users.noreply.github.com>
2021-01-07 10:38:58 +0200

    add keword component

--- seo-react-ui/src/App.js
+++ seo-react-ui/src/App.js
@@ -1,29 +1,18 @@
 
 import { useContext, useState } from "react";
+import { AddKeyword } from "./Components/AddKeyword";
 import { KeywordsContext } from "./Contexts/KeywordsContext";
 
 function App() {
 
-  const [word, setWord] = useState("");
   const {keywords, setKeywords} = useContext(KeywordsContext);
-
-  const updateWord = e =>{
-    setWord(e.target.value);
-  }
-
-  const addKeyword = e =>{
-    e.preventDefault();
-    setKeywords(previousKeywords => [...previousKeywords, {id: 3, keyword: word}]);
-    setWord("");
-  }
 
   return (
       <div>
+          <AddKeyword/>
           {keywords.map(keyword => (
               <h1>{keyword.keyword}</h1>
           ))}
-          <input type = "text" name = "word" value = {word} onChange = {updateWord}></input>   
-          <button onClick = {addKeyword} >ADD KEYWORD</button>
       </div>
   );
 }
--- seo-react-ui/src/Components/AddKeyword.js
+++ seo-react-ui/src/Components/AddKeyword.js
@@ -1,5 +1,36 @@
 
+import { useContext, useState, useEffect } from "react";
+import { KeywordsContext } from "../Contexts/KeywordsContext";
 
-export default class WeatherItem extends Component {
-    
+export const AddKeyword = () =>{
+
+    const [word, setWord] = useState("");
+    const {keywords, setKeywords} = useContext(KeywordsContext);
+  
+   
+    useEffect(() => {
+        if(keywords === null)
+        {   
+            //fetch api
+            setKeywords(previousKeywords => [...previousKeywords, {id: 3, keyword: word}]);
+        }
+    }, [])
+       
+
+    const updateWord = e =>{
+      setWord(e.target.value);
+    }
+  
+    const addKeyword = e =>{
+      e.preventDefault();
+      setKeywords(previousKeywords => [...previousKeywords, {id: 3, keyword: word}]);
+      setWord("");
+    }
+
+    return (
+        <div>
+            <input type = "text" name = "word" value = {word} onChange = {updateWord}></input>   
+            <button onClick = {addKeyword} >ADD KEYWORD</button>
+        </div>
+    );
 }
